6bc0ca856ff136247647607b871051b973f168d4
JunaidKirkire <junaidbkirkire@gmail.com>
2015-10-23 21:09:41 +0530

    Optimizations suggested by Dhimil

--- tail_minus_f/index.js
+++ tail_minus_f/index.js
@@ -4,52 +4,54 @@ var io = require('socket.io')(http);
 var fs = require('fs');
 
 app.get('/', function(req, res){
-  res.sendFile('/Users/Junaid/workspace/nodejs/tail_minus_f/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
+
+function fetchLogChunk(lines) {
+  var logChunk = '';
+  for(var i = 0; i < lines.length; i++) {
+    logChunk += '<li>'+lines[i] + '</li>';
+  }
+  return logChunk;
+}
+
+var last10Lines = [];
+
+console.log(last10Lines);
+
+if(last10Lines.length <= 0) {
+  fs.readFile(process.argv[2], 'utf8', function(err, data) {  
+    if(err) throw err;
+
+    var lines = data.trim().split('\n');
+    noOfLines = lines.length;
+    last10Lines = lines.splice(-10); 
+  });
+}
 
 io.on('connection', function(socket){
   var noOfLines = 0, newNoOfLines = 0;
-  
-  fs.readFile('test.txt', 'utf8', function(err, data) {
-    
-    if(err) throw err;
+
+  socket.emit('log', fetchLogChunk(last10Lines));
+});
+
+fs.open(process.argv[2], 'r', function(err, fd) {
+  fs.watchFile(process.argv[2], function(currStat, prevStat) {
+    var delta = currStat.size - prevStat.size;
+    if (delta <= 0) return;
+
+    fs.read(fd, new Buffer(delta), 0, delta, prevStat.size, function(err, bytes, buffer) {
       
-      var lines = data.trim().split('\n');
-      noOfLines = lines.length;
-      var last10Lines = lines.splice(-10);
-      
-      for(var i = 0; i < last10Lines.length; i++) {
-        socket.emit('chat message', last10Lines[i]);
+      var lastModifiedLines = buffer.toString().trim().split('\n');
+
+      for(var i = 0; i < lastModifiedLines.length; i++) {
+        last10Lines.push(lastModifiedLines[i]);
       }
-  });
+      last10Lines = last10Lines.splice(-10);
 
-  fs.watch('test.txt', function(event, filename) {
-    
-    if( filename ) {
-      fs.createReadStream(filename)
-      .on('data', function(chunk) {
+      io.emit('log', fetchLogChunk(lastModifiedLines));
 
-        newNoOfLines = chunk.toString().trim().split('\n').length;
-        
-        if(newNoOfLines > noOfLines) {
-          var diff = newNoOfLines - noOfLines;
-          
-          var newLines = chunk.toString().trim().split('\n').splice(-diff);
-          
-          for(var i = 0; i < newLines.length; i++) {
-            socket.emit('chat message', newLines[i]);
-          }
-          
-          noOfLines = newNoOfLines;
-        }
-        
-      })
-      .on('end', function() {
-
-      });
-    } else {
-      console.log('no filename');
-    }
+    });
   });
 });
 
